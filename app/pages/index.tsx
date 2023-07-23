ad99023d1c4e071539409bf7ab99582d5174bd3b
sheezu900 <shahzamanlive2@gmail.com>
2023-07-23 19:43:27 +0500

    add one post in database. now working to get data from database

---
+++ app/pages/index.tsx
@@ -0,0 +1,17 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// async function main() {
+//   const posts = await prisma.post.create({
+//   data: {
+//     id: '1',
+//     title: "its working",
+//     content: "loremp ds is working on the",
+//   },
+//   });
+//   console.log(posts);
+//   return posts;
+// }
+
+// main()
